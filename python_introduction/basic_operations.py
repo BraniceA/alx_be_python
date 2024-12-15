9fb1cfc9f3ba7c25a6f2772e56496b842579e68d
Branice <braniceadongo@gmail.com>
2024-12-15 04:04:48 -0800

    Performing simple arithmetic

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+add = number1 + number2
+diff = number1 - number2
+prod = number1 * number2
+print("Addition of", number1, "and", number2, "is", add)
+print("Subtraction of", number1, "and", number2, "is", diff)
+print("Multiplication of", number1, "and", number2, "is", prod)
