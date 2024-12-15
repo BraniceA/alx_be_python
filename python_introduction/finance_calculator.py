d7537630492f717a14e3158389d1f11548b6a337
Branice <braniceadongo@gmail.com>
2024-12-15 05:55:25 -0800

    Advanced task

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,17 @@
+#User Input for Financial Details
+monthly_income = int(input("Enter your mothly income:"))
+monthly_expenses = int(input("Enter your monthly expenses:"))
+
+#Calculate Monthly Savings
+monthly_savings = monthly_income - monthly_expenses
+
+#Project Annual Savings
+#annual interest rate of 5%
+#Projected savings = Monthly Savings * 12 + (Monthly Savings * 12 * 0.05
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+#Output Results
+#user’s monthly savings
+print("Your monthly savings are $",monthly_savings)
+#projected annual savings after including interest
+print("Projected savings after one year, with interest, is: $",projected_savings)
