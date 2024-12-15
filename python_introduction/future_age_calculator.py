117fc205da39e98f4e6e7e7b3749475efdfb607c
Branice <braniceadongo@gmail.com>
2024-12-15 04:53:39 -0800

    Calculating fuuture age

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,6 @@
+# Calculating future age
+currentAge = input("How old are you?")
+        # Current year is 2023
+        age = currentAge + 27
+        print("In 2050, you will be", age, "years old.") 
+
