e13ab594c2cfd13dbffcdca1bf9f944d6e958803
Branice <braniceadongo@gmail.com>
2024-12-21 07:29:58 -0800

    Simple calculator with match case

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 # A simple calculator
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
 operation = input("Choose the operation (+, -, *, /)")
 match operation:
     case "*":
