4c69de229a7068cfda5180929a75555051ce75fd
Branice <braniceadongo@gmail.com>
2024-12-20 23:12:54 -0800

    Corrected weather advice

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,7 +1,4 @@
-# Objective: Utilize conditional statements to guide program execution based on user input regarding weather conditions.
-# Description: Create a Python script named weather_advice.py. This script will ask the user about the current weather conditions and provide clothing recommendations based on the input. This task aims to demonstrate the use of if, elif, and else statements to make decisions in a program.
-
-# Ask the user to input the current weather from a predefined set of conditions: sunny, rainy, or cold.
+# Weather advice
 weather = input("What's the weather like today? (sunny,rainy,cold) : ").lower()
 if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
