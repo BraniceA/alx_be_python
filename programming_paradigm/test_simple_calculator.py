bc03afff05c066aa92bf199e8672aa54aef817a6
Branice <braniceadongo@gmail.com>
2025-01-22 04:24:30 -0800

    More corrections on test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -21,7 +21,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(5, -5), 10)
         self.assertEqual(self.calc.subtract(5, 0), 5)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(10, 5), 50)
         self.assertEqual(self.calc.multiply(10, -5), -50)
         self.assertEqual(self.calc.multiply(-10, 5), -50)
@@ -29,7 +29,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(-5, -5), 25)
         self.assertEqual(self.calc.multiply(-5, 0), 0)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 5), 2)
         self.assertEqual(self.calc.divide(10, -5), -2)
         self.assertEqual(self.calc.divide(-10, 5), -2)
